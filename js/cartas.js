922c3828d25953ead09ec7db06d9c15ce6fc63b1
Marina Francalancia <laemeenrulada@gmail.com>
2022-09-16 12:32:37 -0300

    tarot lol

---
+++ js/cartas.js
@@ -0,0 +1,179 @@
+
+let cartas = new Array(
+{
+    img:'../img/elmalo.jpg',
+    titulo:'The hanged man',
+    descripcion:'Asociado con el autosacrificio y la paciencia ante las adversidades. ',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/CartaJugador.jpg',
+    titulo:'',
+    descripcion:'Las siguientes son de',
+    id:'',
+    dataNumero:''
+},
+{
+    img:'../img/amigos2.jpg',
+    titulo:'The Emperor',
+    descripcion:'Poder mundano, capacidad, confianza, riqueza, estabilidad, autoridad, espirita indomable, padre, hermano, esposo, influencia masculina, presión directa, comunicación, convicción, fuerza, consecución de metas.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/cientifico.jpg',
+    titulo:'The Judgement',
+    descripcion:'Implica conciencia de la individualidad, que cada uno tiene sus propios pensamientos y que se van a producir cambios en los astros con repercusión en la humanidad.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/ekko.jpg',
+    titulo:'Temperance',
+    descripcion:'Implica movimiento hacia la armonía. ',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/elfool.jpg',
+    titulo:'The Fool',
+    descripcion:'Combina sabiduría e insensatez, hace las cosas desordenadamente pero, curiosamente, están bien hechas y es normal que sean así.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/jinx.jpg',
+    titulo:'The World',
+    descripcion:'Representa el esfuerzo, la experiencia, la constancia y el sentido práctico, que culminan en un reconocimiento social.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/elpoli.jpg',
+    titulo:'The Hierophant',
+    descripcion:'Representa a una persona comprometida con lo que quiere, que sabe mantener su integridad a pesar de la crítica, capaz de iluminar a otros con su ejemplo y enseñanzas.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/jinxsad.jpg',
+    titulo:'The Devil',
+    descripcion:'Representa el ser humano que está atado a los deseos materiales, los vicios y la materia.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/latorre.jpg',
+    titulo:'The Tower',
+    descripcion:'Asociada con el caos, la catástrofe y la ruina. ',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/princesa.jpg',
+    titulo:'The Empress',
+    descripcion:'Representa a la fuerza femenina activa y dominante, que actúa directamente para realizar sus fines.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/vi.jpg',
+    titulo:'The Moon',
+    descripcion:'Está asociada con la entidad atómica desconocida, la seguridad en la oscuridad y con el asiento del alma y con la eternidad.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/vibeta.jpg',
+    titulo:'Strength',
+    descripcion:'Simboliza que la voluntad que se antepone a cualquier problema, para encauzarlo y solucionarlo.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/thelovers.jpg',
+    titulo:'The Lovers',
+    descripcion:'Asociado con la necesidad de elección, de los impulsos sexuales y románticos. Es el niño que deja de ser niño y madura, impulsado por la curiosidad o el deber.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/thejustice.jpg',
+    titulo:'The Justice',
+    descripcion:'Es la representación de la armonía, el equilibrio, el aplomo, la estabilidad y la equidad, la asertividad, las decisiones justas, las negociaciones.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/themagician.jpg',
+    titulo:'The Magician',
+    descripcion:'Representa los inicios, la acción. Es la creatividad, la inteligencia, la vitalidad, la astucia y la confianza en uno mismo.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/thedeath.jpg',
+    titulo:'The Death',
+    descripcion:'Representa literalmente la muerte física y no necesariamente es un vaticinio negativo. La Muerte presenta el cambio , el fin de un ciclo y el resurgimiento de otro. Por ende, la muerte no debe ser temida.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/thehermit.jpg',
+    titulo:'The Hermit',
+    descripcion:'Representa la introspección, la meditación en solitario, la necesidad de autoconocimiento, de retiro del mundo para entender mejor lo aprendido y asimilarlo.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+{
+    img:'../img/thechariot.jpg',
+    titulo:'The Chariot',
+    descripcion:'Representa la movilidad, el dinamismo y la rapidez en general. También es el trabajo y el éxito, así como la disciplina y la capacidad para dominar las situaciones y las emociones.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/thesun.jpg',
+    titulo:'The Sum',
+    descripcion:'Refleja felicidad, satisfacción, vitalidad, confianza en uno mismo y exito.',
+    id:'',
+    rol:'dios',
+    dataNumero:''
+},
+{
+    img:'../img/amigos.jpg',
+    titulo:'Wheel of fortune',
+    descripcion:'La Rueda de la Fortuna generalmente representa el vaivén de la vida, el Destino y el Karma.',
+    id:'',
+    rol:'humano',
+    dataNumero:''
+},
+
+
+
+
+
+
+
+
+
+)
